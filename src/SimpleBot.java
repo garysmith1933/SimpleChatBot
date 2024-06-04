bca83adb2f5bd014731414304259bfa13ec99513
Gary Smith <garysmith193358@gmail.com>
2024-06-04 19:28:32 -0400

    bot gives greeting

---
+++ src/SimpleBot.java
@@ -0,0 +1,9 @@
+public class SimpleBot {
+    public static void main(String[] args) {
+        String botName = "johnCena";
+        int birthYear = 2024;
+
+        System.out.println("Hello! My name is " + botName + ".");
+        System.out.println("I was created in " + birthYear + ".");
+    }
+}
